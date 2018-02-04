52dc90860c6aa6ec3643d2e7446718c46db9439c
Denisa <denisaionitaaa@gmail.com>
2018-02-04 14:32:12 +0200

    migration
    - migration

---
+++ ProiectDAW/ProiectDAW/Migrations/20180204122512_Test1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ProiectDAW.Migrations
+{
+    public partial class Test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<string>(nullable: false),
+                    Author = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ISBN = table.Column<string>(nullable: true),
+                    Language = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Year = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GenreLists",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    BookGenre = table.Column<string>(nullable: true),
+                    BookId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GenreLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wishlists",
+                columns: table => new
+                {
+                    WishlistId = table.Column<string>(nullable: false),
+                    BookId = table.Column<string>(nullable: true),
+                    UserEmail = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wishlists", x => x.WishlistId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "GenreLists");
+
+            migrationBuilder.DropTable(
+                name: "Wishlists");
+        }
+    }
+}
--- ProiectDAW/ProiectDaw/Migrations/ApplicationDBContextModelSnapshot.cs
+++ ProiectDAW/ProiectDaw/Migrations/ApplicationDBContextModelSnapshot.cs
@@ -19,9 +19,46 @@ namespace ProiectDAW.Migrations
                 .HasAnnotation("ProductVersion", "2.0.1-rtm-125")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
+            modelBuilder.Entity("ProiectDAW.Models.Book", b =>
+                {
+                    b.Property<string>("BookId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Author");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("ISBN");
+
+                    b.Property<string>("Language");
+
+                    b.Property<string>("Title");
+
+                    b.Property<string>("Year");
+
+                    b.HasKey("BookId");
+
+                    b.ToTable("Books");
+                });
+
+            modelBuilder.Entity("ProiectDAW.Models.GenreList", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("BookGenre");
+
+                    b.Property<string>("BookId");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("GenreLists");
+                });
+
             modelBuilder.Entity("ProiectDAW.Models.User", b =>
                 {
-                    b.Property<string>("EmailAddress");
+                    b.Property<string>("EmailAddress")
+                        .ValueGeneratedOnAdd();
 
                     b.Property<string>("Firstname");
 
@@ -29,60 +66,28 @@ namespace ProiectDAW.Migrations
 
                     b.Property<string>("Password");
 
-                    b.Property<string>("Role");
-                  
                     b.Property<string>("ProfilePicture");
+
+                    b.Property<string>("Role");
 
                     b.HasKey("EmailAddress");
 
                     b.ToTable("Users");
                 });
 
-            modelBuilder.Entity("ProiectDAW.Models.GenreList", b =>
-            {
-                b.Property<string>("Id");
-
-                b.Property<string>("BookGenre");
-
-                b.Property<string>("BookId");
-
-                b.HasKey("Id");
-
-                b.ToTable("GenreLists");
-            });
-
-            modelBuilder.Entity("ProiectDAW.Models.Book", b =>
-            {
-                b.Property<string>("BookId");
-
-                b.Property<string>("Title");
-
-                b.Property<string>("Author");
-
-                b.Property<string>("Year");
-
-                b.Property<string>("Description");
-
-                b.Property<string>("ISBN");
-                b.Property<string>("Language");
-
-                b.HasKey("BookId");
-
-                b.ToTable("Books");
-            });
-
             modelBuilder.Entity("ProiectDAW.Models.Wishlist", b =>
-            {
-                b.Property<string>("WishlistId");
+                {
+                    b.Property<string>("WishlistId")
+                        .ValueGeneratedOnAdd();
 
-                b.Property<string>("UserEmail");
+                    b.Property<string>("BookId");
 
-                b.Property<string>("BookId");
+                    b.Property<string>("UserEmail");
 
-                b.HasKey("WishlistId");
+                    b.HasKey("WishlistId");
 
-                b.ToTable("Wishlists");
-            });
+                    b.ToTable("Wishlists");
+                });
 #pragma warning restore 612, 618
         }
     }
